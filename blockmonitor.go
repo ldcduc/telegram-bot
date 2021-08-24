d6edc249fb454cbdf73ad2bdfe85290a20ff8e61
Duc Le <duc.le@kyber.network>
2021-08-25 03:31:42 +0700

    Keep monitoring alive nodes until everenode's dead

--- blockmonitor.go
+++ blockmonitor.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/urfave/cli"
@@ -40,6 +41,7 @@ func blockHeightMonitor(ctx *cli.Context) {
 	fmt.Printf("Check for every %d seconds\n", second)
 	caption := fmt.Sprintf("Monitoring bot started, checks for every %d seconds\nSend %d messages in maximum if failed", second, MaxTimes)
 	sendAlert(client, startingInfo, caption, true)
+	var deadNode = []int {}
 	for {
 		var tmp = []int {}
 		for i := 0; i < MaxNodes; i ++ {
@@ -47,7 +49,7 @@ func blockHeightMonitor(ctx *cli.Context) {
 		}
 
 		var failedNode = []int {}
-		failedNode = node.Check_last_consensused_height(MaxNodes, last_consensused_block_height, failedNode)
+		failedNode = node.Check_last_consensused_height(MaxNodes, last_consensused_block_height, failedNode, deadNode)
 		fmt.Println("failedNode len", len(failedNode))
 		caption := ""
 		if len(failedNode) > 0 {
@@ -55,8 +57,18 @@ func blockHeightMonitor(ctx *cli.Context) {
 			for i := 0; i < len(failedNode); i ++ {
 				caption = caption + fmt.Sprintf("node=%d: last_consensused_block_height=%d, current=%d\n", failedNode[i], tmp[failedNode[i] - 1], last_consensused_block_height[failedNode[i] - 1])
 			}
-			sendAlert(client, "Found no change in last_consensused_block_height - Alert sent", caption, isLastTimeChanged)
-			fmt.Println("Alert sent: {:?}", caption)
+			sendAlert(client, caption, fmt.Sprintf("Found no change in last_consensused_block_height - Alert sent - Previous dead nodes = %v", deadNode), isLastTimeChanged)
+			fmt.Println("Alert sent:", caption)
+			if !isLastTimeChanged && client.SendCount > MaxTimes { // not forceSend + sent enough messages
+				client.SendCount = 0
+				deadNode = append(deadNode, failedNode...)
+				if len(deadNode) == MaxNodes {
+					sendAlert(client, "All nodes have been dead", "Monitoring bot stopped", isLastTimeChanged)
+					fmt.Println("Monitoring bot stopped: All nodes have been dead")
+					os.Exit(0)
+				}
+				fmt.Println("client.SendCount is reset to 0 - deadNode =", deadNode)
+			}
 		} else {
 			isLastTimeChanged = true
 			currentTime := time.Now()
@@ -69,7 +81,7 @@ func blockHeightMonitor(ctx *cli.Context) {
 func sendAlert(client *Client, msg string, caption string, forceSend bool) {
 	if forceSend {
 		// send message not increase counter
-		fmt.Printf("================send msg: %s", msg)
+		// fmt.Printf("================send msg: %s\n", msg)
 		client.TeleClient.SendMessage(msg, caption)
 		client.SendCount = 1
 		return
@@ -77,7 +89,7 @@ func sendAlert(client *Client, msg string, caption string, forceSend bool) {
 	if client.SendCount > MaxTimes {
 		return
 	}
-	fmt.Printf("================send msg: %s", msg)
+	// fmt.Printf("================send msg: %s\n", msg)
 	client.TeleClient.SendMessage(msg, caption)
 	client.SendCount++
 }
--- node/blockheight.go
+++ node/blockheight.go
@@ -24,34 +24,43 @@ func GetPsqlInfo(offset int) string {
 	return psqlInfo
 }
 
-func Check_last_consensused_height(maxNode int, last_consensused_block_height []int, failedNode []int) []int {
+func Check_last_consensused_height(maxNode int, last_consensused_block_height []int, failedNode []int, deadNode []int) []int {
 	for nodeIndex := 1; nodeIndex <= maxNode; nodeIndex ++ {
-		db, err := sql.Open("postgres", GetPsqlInfo(nodeIndex)) // 9000 + offset
-		if err != nil {
-			panic(err)
+		isDead := false
+		for _, node := range deadNode {
+			if node == nodeIndex {
+				isDead = true	
+				break
+			}
 		}
-		defer db.Close()
+		if !isDead {
+			db, err := sql.Open("postgres", GetPsqlInfo(nodeIndex)) // 9000 + offset
+			if err != nil {
+				panic(err)
+			}
+			defer db.Close()
 
-		err = db.Ping()
-		if err != nil {
-			panic(err)
-		}
+			err = db.Ping()
+			if err != nil {
+				panic(err)
+			}
 
-		fmt.Println("Successfully connected to node ", nodeIndex)
+			fmt.Println("Successfully connected to node ", nodeIndex)
 
-		var blockValue BlockValue
-		blockValueSql := "SELECT key, value FROM block_values"
+			var blockValue BlockValue
+			blockValueSql := "SELECT key, value FROM block_values"
 
-		err = db.QueryRow(blockValueSql).Scan(&blockValue.key, &blockValue.value)
-		if err != nil {
-			log.Fatal("Failed to execute query: ", err)
-		}
+			err = db.QueryRow(blockValueSql).Scan(&blockValue.key, &blockValue.value)
+			if err != nil {
+				log.Fatal("Failed to execute query: ", err)
+			}
 
-		if last_consensused_block_height[nodeIndex - 1] == blockValue.value {
-			fmt.Println("FALSE AT NODE ", nodeIndex)
-			failedNode = append(failedNode, nodeIndex)
-		} else {
-			last_consensused_block_height[nodeIndex - 1] = blockValue.value
+			if last_consensused_block_height[nodeIndex - 1] == blockValue.value {
+				fmt.Println("FALSE AT NODE ", nodeIndex)
+				failedNode = append(failedNode, nodeIndex)
+			} else {
+				last_consensused_block_height[nodeIndex - 1] = blockValue.value
+			}
 		}
 	}
 
